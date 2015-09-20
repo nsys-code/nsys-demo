6325d138eb6269f3da9b61ec192a4f637cd3ae71
Tomas Hrdlicka <tomas@hrdlicka.co.uk>
2015-09-20 03:26:06 +0200

    Updating build script and pom.xml file

--- build.bat
+++ build.bat
@@ -1,3 +1,7 @@
 @echo off
 
-mvn clean resources:resources install
+REM Maven automatically fetches the latest SNAPSHOT on daily basis. 
+REM You can force Maven to download latest snapshot build using -U 
+REM switch to any Maven command.
+
+mvn clean resources:resources install -U
--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-mvn clean resources:resources install
+# Maven automatically fetches the latest SNAPSHOT on daily basis. 
+# You can force Maven to download latest snapshot build using -U 
+# switch to any Maven command.
+
+mvn clean resources:resources install -U
